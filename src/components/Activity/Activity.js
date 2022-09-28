882aee81e859eaf3c88dd67e7aa1e5500818d13d
Sehrish <sehrishzeba012@gmail.com>
2022-09-29 04:38:56 +0600

    2nd

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -4,18 +4,15 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Activity.css';
 
 const Activity = ({ activity, handleAddToCart }) => {
-    const { name, img, time } = activity;
+    const { name, img, time, description, age } = activity;
     return (
         <div className='activity'>
             <img src={img} alt="" />
             <div className="activity-info">
                 <p className='activity-name'>{name}</p>
-                <p><small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam optio magni incidunt, aliquid qui molestiae!</small></p>
-                <p>For age: 20-25</p>
-                {/* time= time */}
+                <p><small>{description}</small></p>
+                <p>For age: {age}</p>
                 <p>Time required:{time}s</p>
-                {/* <p><small>Time required:{seller}</small></p> */}
-                {/* <p><small>Ratings:{ratings} stars</small></p> */}
             </div>
             <button onClick={() => handleAddToCart(activity)} className='btn-cart'>
                 <p className='btn-text'>Add to list</p>
